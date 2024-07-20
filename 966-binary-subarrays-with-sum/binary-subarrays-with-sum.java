f626eaa5e581b82c354f999a9264d230bfcf6893
sakandd <109812183+sakandd@users.noreply.github.com>
2024-07-20 11:39:05 +0530

    Time: 22 ms (37.34%) | Memory: 52.9 MB (5.43%) - LeetSync

---
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSubarraysWithSum(int[] nums, int goal) {
+        Map<Integer, Integer> sumCount = new HashMap<>();
+        sumCount.put(0, 1); // Base case: there's one way to have a sum of 0 (with an empty subarray)
+        int currentSum = 0;
+        int result = 0;
+
+        for (int num : nums) {
+            currentSum += num;
+
+            // If currentSum - S exists in the map, it means there are subarrays that sum to S
+            if (sumCount.containsKey(currentSum - goal)) {
+                result += sumCount.get(currentSum - goal);
+            }
+
+            // Update the frequency of the currentSum in the map
+            sumCount.put(currentSum, sumCount.getOrDefault(currentSum, 0) + 1);
+        }
+
+        return result;
+    }
+}
