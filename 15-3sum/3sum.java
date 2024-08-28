005c9d08230fcae887b39f35844f5769c2dfec17
sakandd <109812183+sakandd@users.noreply.github.com>
2024-08-29 00:37:27 +0530

    Time: 869 ms (10.93%) | Memory: 52.6 MB (17.63%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,35 +1,26 @@
-
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        int n = nums.length;
-
-        for (int i = 0; i < n; i++) {
-            // Skip duplicates for the first element of the triplet
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            // Use two pointers to find the other two elements
-            int j = i + 1;
-            int k = n - 1;
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if (sum < 0) {
-                    j++;
-                } else if (sum > 0) {
-                    k--;
-                } else {
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    k--;
-                    // Skip duplicates for the second element of the triplet
-                    while (j < k && nums[j] == nums[j - 1]) j++;
-                    // Skip duplicates for the third element of the triplet
-                    while (j < k && nums[k] == nums[k + 1]) k--;
+        Set<List<Integer>>result = new HashSet<>();
+        for(int i = 0; i<nums.length-2;i++){
+            int start = i+1;
+            int end = nums.length-1;
+            while(start<end){
+                int sum = nums[i]+ nums[start]+ nums[end];
+                if(sum == 0){
+                    result.add(Arrays.asList(nums[i],nums[start],nums[end]));
+                    start++;
+                    end--;
+                }
+                else if(sum<0){
+                    start++;
+                }else{
+                    end--;
+    
                 }
             }
+           
         }
-
-        return ans;
+         return new ArrayList<>(result);
     }
 }
