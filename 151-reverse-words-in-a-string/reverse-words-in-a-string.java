263245be052f9f590d5e4d224c4dc837904d2960
sakandd <109812183+sakandd@users.noreply.github.com>
2025-02-07 22:36:59 +0530

    Time: 7 ms (59.65%) | Memory: 43.4 MB (43.84%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,35 +1,16 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<String> st = new Stack<>();
-        String str = "";
 
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') {
-                if (!str.isEmpty()) {
-                    st.push(str);
-                    str = "";
-                }
-            } else {
-                str += s.charAt(i);
-            }
+        String[]words = s.trim().split("\\s+");
+        int start = 0;
+        int end = words.length-1;
+        while(start<end){
+            String temp = words[start];
+            words[start] = words[end];
+            words[end] = temp;
+            start++;
+            end--;
         }
-        
-        // Push the last word to the stack
-        if (!str.isEmpty()) {
-            st.push(str);
-        }
-
-        String ans = "";
-        while (st.size() > 1) {
-            ans += st.peek() + " ";
-            st.pop();
-        }
-        // Append the last word without a trailing space
-        if (!st.isEmpty()) {
-            ans += st.peek();
-            st.pop();
-        }
-
-        return ans;
+        return String.join(" " , words);
     }
 }
